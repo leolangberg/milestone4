f8bc6b266d5f334e4ca2d51c9697f958fc1e2c4e
Leo Långberg <152855963+leolangberg@users.noreply.github.com>
2025-10-14 02:43:16 +0200

    different type of mem_sys

--- milestone4/milestone4.srcs/sources_1/new/mile3.sv
+++ milestone4/milestone4.srcs/sources_1/new/mile3.sv
@@ -1,67 +1,79 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 14.10.2025 01:29:41
+// Design Name: 
+// Module Name: mem_sys
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
 
-module memory_system (
-	input  logic        clk,
-	input  logic        rst_n,
 
-	// CPU side (byte-addressed)
-	input  logic [31:0] addr,       // byte address; word index = addr[15:2]
-	input  logic        read_en,    // hold high until mem_ready
-	input  logic        write_en,   // hold high until mem_ready
-	input  logic [31:0] data_in,
-	output logic [31:0] data_out,
-	output logic        mem_ready
+module mem_sys(
+      input  logic        clk,
+      input  logic        rst_n,
+    
+      // CPU side (byte-addressed)
+      input  logic [31:0] addr,       // byte address; word index = addr[15:2]
+      input  logic        read_en,    // hold high until mem_ready
+      input  logic        write_en,   // hold high until mem_ready
+      input  logic [31:0] data_in,
+      output logic [31:0] data_out,
+      output logic        mem_ready
 );
 
-wire [13:0] addra_w = addr[15:2];    // 64 KiB / 4B = 16K words -> 14-bit
-  
-typedef enum logic [1:0] {IDLE, LOAD, READ, DONE} state;
-state prev_state, next_state;
-always_ff @(posedge clk or negedge rst_n) begin
-	if (!rst_n) prev_state <= IDLE;
-	else        prev_state <= next_state;
-end
-
-
-always_comb begin
-	next_state = IDLE;
-	case (prev_state)
-		IDLE : begin
-			if(read_en == 1) begin
-				next_state = LOAD;
-			end else begin
-				next_state = IDLE;
-			end
-		end
-		LOAD : begin
-			next_state = READ;
-		end
-		READ : begin
-			// We issued read and so memory should be 
-			// read on next cycle
-			next_state = DONE;
-			ip_en = 1;
-		end
-		DONE : begin
-			next_state = IDLE;
-			mem_ready = 1'b1;
-		end
-	endcase
-end
-
-logic        ip_ena;
-logic [13:0] ip_addra;
-logic [31:0] ip_douta;
-logic [31:0] ip_dina;
-logic [3:0] ip_wea;
-assign data_out = ip_douta;
-assign ip_addra = addra_w;
+wire [13:0] addra_w = addr[15:2]; 
+logic en;
+logic [3:0] wea_mask;
 
 sram u_sram (
     .clka  (clk),
-    .ena   (ip_ena),
-    .wea   (ip_wea),         // 4 bits
-    .addra (ip_addra),
-    .dina  (ip_dina),
-    .douta (ip_douta)
+    .ena   (en),
+    .wea   (wea_mask),         // 4 bits
+    .addra (addra_w),
+    .dina  (data_in),
+    .douta (data_out)
   );
-endmodule;
+
+typedef enum logic [1:0] {IDLE, WAIT, DONE} state_t;
+  state_t pstate, nstate;
+  
+ always_ff @(posedge clk or negedge rst_n) begin
+    if (!rst_n) pstate <= IDLE;
+    else        pstate <= nstate;
+  end
+  
+  always_comb begin 
+    en = read_en | write_en;
+    wea_mask = 4'b0000;
+    case (pstate) 
+        IDLE: if(read_en) begin
+                    nstate = WAIT;
+              end
+              else if(write_en) begin
+                    wea_mask = 4'b1111;
+                    nstate = DONE;
+              end else
+                    nstate = IDLE;
+        WAIT: nstate = DONE;
+        DONE: nstate = IDLE;
+        default: nstate = IDLE;
+    endcase
+  end
+
+assign mem_ready = (pstate == DONE);
+
+
+
+endmodule
